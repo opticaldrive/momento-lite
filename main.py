752fb2cd94f3f1e0a9d3d7eccb26644f1c652b15
a <138826219+opticaldrive@users.noreply.github.com>
2026-02-16 00:16:55 -0800

    im dumb oop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import uuid
 app = FastAPI()
 
 
@@ -11,3 +11,42 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+
+
+class Scan:
+    
+    def __init__(self, uuid, url:str, visibility:str):
+        self.uuid = str(uuid.uuid4())
+        self.url = str(url)
+        # self.status_code = 0 # 
+        # self.status = "D"
+        self.visibility = "public"
+        pass
+
+    # def delete_scan(self):
+    
+@app.post("/api/v1/scan")
+def create_scan(url:str,visibility="public":str | None = None):
+    # return {"item_id": item_id, "q": q}
+
+    # do stuff
+    return {
+            "uuid":123,
+            "country":"ab", 
+            "visibility": visibility,#"public",
+            "url":url#"https://asd.com"
+    }
+
+
+@app.post("/api/v1/scan")
+def create_scan(url:str,visibility:str="public" | None = None):
+    # return {"item_id": item_id, "q": q}
+
+    # do stuff
+    return {
+            "uuid":123,
+            "country":"ab", 
+            "visibility": "public",
+            "url":"https://asd.com"
+    }
